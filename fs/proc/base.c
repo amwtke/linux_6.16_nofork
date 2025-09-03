d9a334b99deb8104b9a8921a018d0ba2832aa392
amwtke <amwtke@gmail.com>
2025-09-03 09:55:38 +0800

    proc/maps

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -3685,6 +3685,7 @@ static const struct pid_entry tid_base_stuff[] = {
 	REG("cmdline",   S_IRUGO, proc_pid_cmdline_ops),
 	ONE("stat",      S_IRUGO, proc_tid_stat),
 	ONE("statm",     S_IRUGO, proc_pid_statm),
+	//!xiaojin /proc/pid/maps
 	REG("maps",      S_IRUGO, proc_pid_maps_operations),
 #ifdef CONFIG_PROC_CHILDREN
 	REG("children",  S_IRUGO, proc_tid_children_operations),
